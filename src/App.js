1fe62d3f58c21f1e120842a66e9063107c127d3a
Java With Nikhil <nikhilmimroth2@gmail.com>
2023-02-24 13:12:23 +0530

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Slider from './CustomComponent/Slider' 
-import SidebarItem from './CustomComponent/SidebarItem' 
+import Slider from './CustomComponent/Slider'
+import SidebarItem from './CustomComponent/SidebarItem'
 
 const DEFAULT_OPTIONS = [
   {
@@ -26,7 +26,7 @@ const DEFAULT_OPTIONS = [
   },
   {
     name: 'Saturation',
-    property: 'saturation',
+    property: 'saturate',
     value: 100,
     range: {
       min: 0,
@@ -82,21 +82,44 @@ function App() {
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionIndex]
 
+  function handleSliderChange({ target }){
+    setOptions(prev => {
+      return prev.map((a, index) => {
+        if(index !== selectedOptionIndex) return a
+        return { ...a, value: target.value }
+      })
+    })
+  }
+
+  function getImageStyle(){
+    const filters = options.map(opt => {
+      return `${opt.property}(${opt.value}${opt.unit})`
+    })
+
+    return { filter: filters.join(' ') }
+  }
+
   return (
     <div className="container">
-      <div className="main-image"></div>
+      <div className="main-image" style={getImageStyle()}></div>
       <div className="sidebar">
         {options.map((ele, index) => {
           return (
-            <SidebarItem 
+            <SidebarItem
             key={index}
             name={ele.name}
             active={index === selectedOptionIndex}
+            handleClick={(() => setSelectedOptionIndex(index))}
             />
           )
         })}
       </div>
-      <Slider/>
+      <Slider
+        min={selectedOption.range.min}
+        max={selectedOption.range.max}
+        value={selectedOption.value}
+        handleChange={handleSliderChange}
+      />
     </div>
   );
 }
--- src/CustomComponent/SidebarItem.js
+++ src/CustomComponent/SidebarItem.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function SidebarItem({ name, active, handleClick }) {
   return (
-    <button className={`sidebar-item ${active ? 'active' : ''}`}>{name}</button>
+    <button 
+      className={`sidebar-item ${active ? 'active' : ''}`}
+      onClick={handleClick}
+    >
+      {name}
+    </button>
   )
 }
