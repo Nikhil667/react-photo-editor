49d16140a63d940f46d793dca2a73e8e3a6a1246
Java With Nikhil <nikhilmimroth2@gmail.com>
2023-02-24 16:13:40 +0530

    Added Upload button and Download button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import Slider from './CustomComponent/Slider'
 import SidebarItem from './CustomComponent/SidebarItem'
+import defaultImage from './temp/img.jpg'
+import * as htmlToImage from 'html-to-image';
 
 const DEFAULT_OPTIONS = [
   {
@@ -81,6 +83,8 @@ function App() {
   const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
   const [options, setOptions] = useState(DEFAULT_OPTIONS)
   const selectedOption = options[selectedOptionIndex]
+  const [image, setImage] = useState(defaultImage)
+  const domEl = useRef(null)
 
   function handleSliderChange({ target }){
     setOptions(prev => {
@@ -99,29 +103,52 @@ function App() {
     return { filter: filters.join(' ') }
   }
 
-  const getImage = {backgroundImage: "url('https://images.unsplash.com/photo-1620553967922-f3ca60bbc1ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"};
+  function uploadImage(e){
+    setImage(URL.createObjectURL(e.target.files[0]))
+  }
+
+  const getImage = {backgroundImage: `url(${image})`};
+
+  const downloadImage = async () => {
+    const dataUrl = await htmlToImage.toJpeg(domEl.current);
+
+    // download image
+    const link = document.createElement('a');
+    link.download = 'download.jpg';
+    link.href = dataUrl;
+    link.click();
+  };
+
+  console.log(domEl)
 
   return (
     <div className="container">
-      <div className="main-image" style={Object.assign(getImageStyle(), getImage)}></div>
+      <div
+        className="main-image"
+        style={Object.assign(getImageStyle(), getImage)}
+        ref={domEl}
+      >
+      </div>
       <div className="sidebar">
         {options.map((ele, index) => {
           return (
             <SidebarItem
-            key={index}
-            name={ele.name}
-            active={index === selectedOptionIndex}
-            handleClick={(() => setSelectedOptionIndex(index))}
+              key={index}
+              name={ele.name}
+              active={index === selectedOptionIndex}
+              handleClick={() => setSelectedOptionIndex(index)}
             />
-          )
+          );
         })}
       </div>
-      <Slider
-        min={selectedOption.range.min}
-        max={selectedOption.range.max}
-        value={selectedOption.value}
-        handleChange={handleSliderChange}
-      />
+        <Slider
+          min={selectedOption.range.min}
+          max={selectedOption.range.max}
+          value={selectedOption.value}
+          handleChange={handleSliderChange}
+          handleImage={uploadImage}
+          downloadImage={downloadImage}
+        />
     </div>
   );
 }
--- src/CustomComponent/Slider.js
+++ src/CustomComponent/Slider.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-export default function Slider({ min, max, value, handleChange }) {
+export default function Slider({ min, max, value, handleChange, handleImage, downloadImage }) {
+  
   return (
     <div className='slider-container'>
+      <div className='menu-btn'>
+        <input 
+          type="file" 
+          className='upload-btn' 
+          onChange={handleImage}
+          />
+        <button 
+          type="button"
+          onClick={downloadImage}
+          className="btn"
+          >
+            Download
+        </button>
+      </div>
       <input 
         type="range" 
         className='slider'
